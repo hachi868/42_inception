46f458e8ed53a047048325126e859fe37bd8c71c
hachi868 <24986912+hachi868@users.noreply.github.com>
2023-12-11 02:44:18 +0900

    :bug:[fix]mariadb start, init

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -5,18 +5,23 @@ if [ ! -d "/run/mysqld" ]; then
 	chown -R mysql:mysql /run/mysqld
 fi
 
+# MariaDBサーバーをバックグラウンドで起動
+mysqld &
+
 # MariaDBサーバーが起動するまで待つ
 while ! mysqladmin ping -h localhost --silent; do
-    sleep 1
+  sleep 1
 done
 
-exec mysqld
-
+# データベースが存在しない場合にのみ初期設定を実行
+if ! mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "use $MARIADB_DATABASE"; then
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;
-    CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';
-    GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%';
-    FLUSH PRIVILEGES;
+  CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;
+  CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';
+  GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%';
+  FLUSH PRIVILEGES;
 EOSQL
+fi
 
-
+# フォアグラウンドでサーバーを維持する
+wait $!
