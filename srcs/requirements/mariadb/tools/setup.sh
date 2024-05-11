f2840e244ef2f1a922df17afef3712e46ede96f0
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-05-12 05:04:26 +0900

    :bug:[fix]mariaDBをフォアグラウンド起動に

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -5,23 +5,53 @@ if [ ! -d "/run/mysqld" ]; then
 	chown -R mysql:mysql /run/mysqld
 fi
 
-# MariaDBサーバーをバックグラウンドで起動
-mysqld &
+start_server() {
+  # MariaDBサーバー起動（フォアグラウンド）
+  mysqld
 
-# MariaDBサーバーが起動するまで待つ
-while ! mysqladmin ping -h localhost --silent; do
-  sleep 1
-done
+  # MariaDBサーバーが起動するまで待つ(max30秒)
+  timeout=30
+  timewait=0
+  while ! mysqladmin ping -h localhost --silent; do
+    sleep 1
+    ((timewait++))
+
+    if [ "${timewait}" -ge "${timeout}" ]; then
+      echo "MariaDB server did not start within ${timeout} seconds."
+      # MariaDBプロセスを探し出して終了
+      pkill -f mysqld
+      exit 1
+    fi
+  done
+
+  echo "MariaDB server started successfully."
+}
+
+shutdown_server() {
+  mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+
+  # コマンドの実行結果を検証
+  if [ $? -eq 0 ]; then
+      echo "MariaDB has been stopped successfully."
+  else
+      echo "Failed to stop MariaDB."
+      exit 1
+  fi
+}
+
+# 初期化が必要かチェックするため、起動=>DB確認、必要なら初期化=>shutdown
+start_server()
 
 # データベースが存在しない場合にのみ初期設定を実行
 if ! mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "use ${MARIADB_DATABASE}"; then
 mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<-EOSQL
-  CREATE DATABASE IF NOT EXISTS "${MARIADB_DATABASE}";
-  CREATE USER IF NOT EXISTS "${MARIADB_USER}"@"%" IDENTIFIED BY "${MARIADB_PASSWORD}";
-  GRANT ALL PRIVILEGES ON "${MARIADB_DATABASE}".* TO "${MARIADB_USER}"@"%";
-  FLUSH PRIVILEGES;
-EOSQL
+    CREATE DATABASE IF NOT EXISTS "${MARIADB_DATABASE}";
+    CREATE USER IF NOT EXISTS "${MARIADB_USER}"@"%" IDENTIFIED BY "${MARIADB_PASSWORD}";
+    GRANT ALL PRIVILEGES ON "${MARIADB_DATABASE}".* TO "${MARIADB_USER}"@"%";
+    FLUSH PRIVILEGES;
+  EOSQL
 fi
 
-# フォアグラウンドでサーバーを維持する
-wait $!
+shutdown_server()
+
+exec "$@"
