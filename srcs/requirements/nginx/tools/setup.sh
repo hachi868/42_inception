c41935dfdbad89bbff201512925a2324090849bd
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-05-12 04:03:30 +0900

    :dizzy:[update]変数の記法を"${}"に統一

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# define
+SSL_DIR="/etc/nginx/ssl"
+KEY_FILE="${SSL_DIR}/inception42.key"
+CERT_FILE="${SSL_DIR}/inception42.crt"
+
 # SSL証明書と秘密鍵の保存先ディレクトリがなければ作成
-mkdir -p /etc/nginx/ssl
+mkdir -p "${SSL_DIR}"
+
+# 証明書
+
 
 # 証明書がなければ作成
-if [ ! -f "/etc/nginx/ssl/inception42.crt" ]; then
+if [ ! -f "${KEY_FILE}" ] || [ ! -f "${CERT_FILE}" ]; then
   openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
       -keyout /etc/nginx/ssl/inception42.key \
       -out /etc/nginx/ssl/inception42.crt \
-      -subj "/C=JP/ST=Tokyo/L=city/O=42tokyo/CN=$DOMAIN_NAME"
+      -subj "/C=JP/ST=Tokyo/L=city/O=42tokyo/CN=${DOMAIN_NAME}"
 fi
 exec "$@"
