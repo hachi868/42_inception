e36f541894dc9d412cc690cbf4143d2fa33c0b87
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-05-19 14:33:40 +0900

    :robot:[fix]ssl証明書 権限

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -11,8 +11,9 @@ mkdir -p "${SSL_DIR}"
 # 証明書がなければ作成
 if [ ! -f "${KEY_FILE}" ] || [ ! -f "${CERT_FILE}" ]; then
   openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-      -keyout /etc/nginx/ssl/inception42.key \
-      -out /etc/nginx/ssl/inception42.crt \
-      -subj "/C=JP/ST=Tokyo/L=city/O=42tokyo/CN=${DOMAIN_NAME}"
+    -keyout /etc/nginx/ssl/inception42.key \
+    -out /etc/nginx/ssl/inception42.crt \
+    -subj "/C=JP/ST=Tokyo/L=city/O=42tokyo/CN=${DOMAIN_NAME}"
+  chmod +r /etc/nginx/ssl/inception42.key /etc/nginx/ssl/inception42.crt
 fi
 exec "$@"
